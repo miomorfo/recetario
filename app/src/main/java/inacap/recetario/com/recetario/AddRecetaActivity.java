7f971d6a2d503284618f53687dd451b38e304aba
Fernando <fernandoebarraza@gmail.com>
2018-04-19 19:38:23 -0300

    Insertar recetas!

--- app/src/main/java/inacap/recetario/com/recetario/AddRecetaActivity.java
+++ app/src/main/java/inacap/recetario/com/recetario/AddRecetaActivity.java
@@ -2,12 +2,53 @@
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class AddRecetaActivity extends AppCompatActivity {
+import inacap.recetario.com.recetario.POJOS.Receta;
+
+ public class AddRecetaActivity extends AppCompatActivity {
+
+    EditText id, nombre, personas, descripcion, preparacion,fav;
+    Button add;
+    Receta receta;
+    Data data;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_receta);
+
+        id = (EditText)findViewById(R.id.id);
+        personas = (EditText)findViewById(R.id.personas);
+        nombre = (EditText)findViewById(R.id.nombre);
+        descripcion = (EditText)findViewById(R.id.descripcion);
+        preparacion = (EditText)findViewById(R.id.preparacion);
+        fav = (EditText)findViewById(R.id.fav);
+        add = (Button) findViewById(R.id.add);
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                receta = new Receta(id.getText().toString(), nombre.getText().toString(),
+                                    Integer.valueOf(personas.getText().toString()),
+                                    descripcion.getText().toString(),
+                                    preparacion.getText().toString(),
+                                    "imagen.jpg",
+                                    Integer.valueOf(fav.getText().toString()));
+
+                data = new Data(getApplicationContext());
+                data.open();
+                data.insertReceta(receta);
+                Toast.makeText(getApplicationContext(),
+                        "Se agrego la receta",
+                        Toast.LENGTH_LONG).show();
+                finish();
+            }
+        });
+
+
     }
 }
